784d7fcb46179fa5fe25ad19444fea84e9031d95
Rodri <rodripb4@hotmail.com>
2022-08-31 20:38:15 +0200

    Trabajando con interceptores

---
+++ src/main/java/com/bolsaideas/springboot/horariointerceptor/app/SrpingBootHorarioApplication.java
@@ -0,0 +1,13 @@
+package com.bolsaideas.springboot.horariointerceptor.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingBootHorarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingBootHorarioApplication.class, args);
+	}
+
+}
